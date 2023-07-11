d34ff72dc420dc67ab1e84dac7adafdb1c5f0026
adnaan-id <adnandikko@gmail.com>
2023-07-11 22:50:10 +0100

    finally completed FizzBuzz challenge! Created new files and a directory, adjusted directory for loops.js

---
+++ js-manipulation/script.js
@@ -0,0 +1,31 @@
+/* this code prints the numbers from 1-100, with Fizz instead of multiples of 3, Buzz instead of multiples of 5 and FizzBuzz for multiples of both */
+
+const X3 = "Fizz";
+const X5 = "Buzz";
+const X3X5 = "FizzBuzz";
+function fizzBuzz (mult3, mult5, both) {
+	const arr = [];
+	for (let i = 1; i <= 100; i++) {
+		arr.push(i);
+	}
+	for (let i = 0; i < arr.length; i++) {
+		const by3 = arr[i] % 3;
+		const by5 = arr[i] % 5;
+		const by3by5 = arr[i] % 3 && arr[i] % 5;
+
+		if (by3 === 0 && by5 === 0) {
+                        arr.splice(i, 1);
+                        arr.splice(i, 0, both);
+                } else if (by3 === 0) {
+			arr.splice(i, 1);
+			arr.splice(i, 0, mult3);
+		} else if (by5 === 0) {
+			arr.splice(i, 1);
+			arr.splice(i, 0, mult5);
+		}
+	}
+	console.log(arr);
+	return;
+}
+
+fizzBuzz(X3, X5, X3X5);
